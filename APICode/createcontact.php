9f1c5e023c5792d50015e544bb0323fbc8f74cf7
Faizar Ali <alifaizar@gmail.com>
2020-05-24 06:55:59 +0300

    a

---
+++ APICode/createcontact.php
@@ -0,0 +1,51 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$name = $inData["name"];
+	$email = $inData["email"];
+	$city = $inData["city"];
+	$state = $inData["state"];
+	$zip = $inData["zip"];
+	$phone = $inData["phone"];
+	$infected = $inData["infected"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "faizar", "", "coronacontacts");
+
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+
+	else
+	{
+		$sql = "insert into Contacts (UserId, Name, Email, City, State, Zip, Phone, Infected) VALUES (" . $userId . ",'" . $name . "','" . $email . "','" . $city . "','" . $state . "','" . $zip . "','" . $phone . "','" . $infected . "')";
+		
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		
+		$conn->close();
+	}
+	
+	returnWithError("CONTACT CREATED!");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
