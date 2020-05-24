dc7742b0539208a30ddb8ecdf4347052ba26cb18
Faizar Ali <alifaizar@gmail.com>
2020-05-24 22:42:02 +0300

    anything

--- APICode/searchcontact.php
+++ APICode/searchcontact.php
@@ -25,7 +25,7 @@
 	{
 		// $sql = "SELECT contact_id, first_name, last_name, email, phone_number, user_id FROM list_of_contacts WHERE (first_name LIKE '%$search%' OR last_name LIKE '%$search%' OR email LIKE '%$search%' OR phone_number LIKE '%$search%') AND user_id = $userId ORDER BY $filter"; 
 
-		$sql = "SELECT * FROM Contacts WHERE ((name LIKE '%$search%') OR (email LIKE '%$search%') OR (city LIKE '%$search%') OR (state LIKE '%$search%') OR (zip LIKE '%$search%') OR (phone LIKE '%$search%') AND user_id = $userId) ORDER BY $filter";
+		$sql = "SELECT * FROM Contacts WHERE ((name LIKE '%$search%') AND (email LIKE '%$search%') AND (city LIKE '%$search%') AND (state LIKE '%$search%') AND (zip LIKE '%$search%') AND (phone LIKE '%$search%')) AND user_id = $userId ORDER BY $filter";
 		
 		$result = $conn->query($sql);
 		
