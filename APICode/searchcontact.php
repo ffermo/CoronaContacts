3e9b8b8e17373b2c01537b44e5462ad9e058f622
Faizar Ali <alifaizar@gmail.com>
2020-05-25 05:09:32 +0300

    anything

--- APICode/searchcontact.php
+++ APICode/searchcontact.php
@@ -58,7 +58,7 @@
 				//				  '"' . $row["Email"] . '"' .
 				//				  '"' . $row["Phone"] . '"';
 
-				$searchResults .= '"' . $row["Name"] . '"' . "\n" . '"' . $row["Email"] . '"' . "\n" . '"' . $row["Phone"] . '"';
+				$searchResults .= "\n" . '"' . $row["Name"] . '"' . "\n" . '"' . $row["Email"] . '"' . "\n" . '"' . $row["Phone"] . '"';
 			}
 		}
 
