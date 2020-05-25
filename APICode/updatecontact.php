c2cd18297416302fe01b37f6cd91a4811d381301
Faizar Ali <alifaizar@gmail.com>
2020-05-25 07:08:58 +0300

    anything

---
+++ APICode/updatecontact.php
@@ -0,0 +1,53 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$name = $inData["name"];
+	$email = $inData["email"];
+	$city = $inData["city"];
+	$state = $inData["state"];
+	$zip = $inData["zip"];
+	$phone = $inData["phone"];
+	$infected = $inData["infected"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "faizar", "", "coronacontacts");
+	
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	
+	else 
+	{
+		// To update info already in a table.: UPDATE [table name] SET Select_priv = 'Y',Insert_priv = 'Y',Update_priv = 'Y' where [field name] = 'user';
+
+		$sql = "insert into Contacts (Name, Email, City, State, Zip, Phone, Infected, UserId) VALUES ('$name','$email','$city','$state','$zip','$phone', '$infected', '$userId')";
+		
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		
+		$conn->close();
+	}
+	
+	returnWithError("CONTACT CREATED!");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
