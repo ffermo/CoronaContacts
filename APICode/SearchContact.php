49bed5c9fcc6429ee9f6a95a89541416129421ce
tugitit <tule1102@gmail.com>
2020-05-27 16:21:26 -0400

    Still testing for rendering on php

--- APICode/SearchContact.php
+++ APICode/SearchContact.php
@@ -118,8 +118,8 @@
 	
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -139,4 +139,3 @@
 <body>
     
 </body>
-</html>
