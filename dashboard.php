cf1440dd885e3f15eea19fa400790a832189b4c7
tugitit <tule1102@gmail.com>
2020-05-27 21:02:22 -0400

    testing again

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://bootswatch.com/4/journal/bootstrap.min.css" />
-    <?php echo "<script type='text/javascript' src='js/code.js'></script>"; ?>
     <title>Dashboard</title>
     <div class="jumbotron">
         <h1 class="display-4">Hello, UserName!</h1>
@@ -38,7 +37,7 @@ $array = array(1, 2, 3, 4);
 </tbody>
 </table>
   </p>
- 
+  <?php echo "<script type='text/javascript' src='js/code.js'></script>"; ?>
   <input id="info" type="button" value="Customer Info">
 <div id="myDiv">
     <form>Name:
