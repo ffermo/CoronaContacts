9006005ccb65453af13c4a4effb8ed9333e648d8
tugitit <tule1102@gmail.com>
2020-05-28 00:02:52 -0400

    added readCookie function changed layout of search

--- js/code.js
+++ js/code.js
@@ -113,14 +113,48 @@ function saveCookie()
 	    var minutes = 20;
 	    var date = new Date();
 	    date.setTime(date.getTime()+(minutes*60*1000));
-	    document.cookie = "first=" + firstName + ",last=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+	    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
 }
 
 function searchContact()
 {
+	alert("Search Utilitzed");
 	var srch = document.getElementById("searchText").value;
 	document.getElementById("searchResults").innerHTML = "";
-	
+
 	var colorList = "";
 	
 	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
@@ -151,8 +185,6 @@ function searchContact()
 			}
 		};
 		xhr.send(jsonPayload);
-
-		alert("Search Utilitzed");
 
 	}
 	catch(err)
