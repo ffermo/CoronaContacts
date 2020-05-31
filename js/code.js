cd91ca185b28019ee7baf1c43822a53b348934c8
tugitit <tule1102@gmail.com>
2020-05-31 16:38:10 -0400

    delete function

--- js/code.js
+++ js/code.js
@@ -175,7 +175,150 @@ function addContact()
 	}
 }
 
+
+function searchContact()
+{
+	var srch = document.getElementById("searchText").value;
+
+	// Json DB elements
+	var table = document.getElementById("contactTable");
+	var name = "";
+	var email = "";
+	var city = "";
+	var state = "";
+	var zip = "";
+	var phoneNumber = "";
+	var infected = "";
+	var datecontactcreated = "";
+	var contactId = "";
 	
+	// Converts to JSON package + sends to the API
+	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
+	var url = urlBase + '/SearchContact.' + extension;
+	
+	// API Call
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{	
+				var jsonObject = JSON.parse( xhr.responseText );
+
+				// Text displays to inform client the search results are displayed. 
+				if (jsonObject.results.length == 0)
+					document.getElementById("searchResults").innerHTML = "No contacts found!";
+				else if (jsonObject.results.length == 1)
+					document.getElementById("searchResults").innerHTML = "1 contact found!";
+				else if (jsonObject.results.length > 1)
+					document.getElementById("searchResults").innerHTML = jsonObject.results.length + " Contacts found!";
+
+
+				for( var i=0; i<jsonObject.results.length; i++ )
+				{				
+					// Parsing JSON objects
+					name = jsonObject.results[i].name;
+					email = jsonObject.results[i].email;
+					city = jsonObject.results[i].city;
+					state = jsonObject.results[i].state;
+					zip = jsonObject.results[i].zip;
+					phoneNumber = jsonObject.results[i].phoneNumber;
+					datecontactcreated = jsonObject.results[i].datecontactcreated;
+					infected = jsonObject.results[i].infected;
+					contactId = jsonObject.results[i].contactId;
+
+					// Inserting from the bottom
+					var row = table.insertRow(-1);
+					
+					// Creating a Cell into Position
+					var cell0 = row.insertCell(0);
+					var cell1 = row.insertCell(1);
+					var cell2 = row.insertCell(2);
+					var cell3 = row.insertCell(3);
+					var cell4 = row.insertCell(4);
+					var cell5 = row.insertCell(5);
+					var cell6 = row.insertCell(6);
+					var cell7 = row.insertCell(7);
+					var cell8 = row.insertCell(8);
+					
+					// Data Renders variables on HTML
+					cell0.innerHTML = name;
+					cell1.innerHTML = email;
+					cell2.innerHTML = city;
+					cell3.innerHTML = state;
+					cell4.innerHTML = zip;
+					cell5.innerHTML = phoneNumber;
+					cell6.innerHTML = infected;
+					cell7.innerHTML = datecontactcreated;
+					cell8.innerHTML = '<button type="button" class="btn btn-primary">Edit</button><button type="button" class="btn btn-dark" onclick=deleteContact(' + contactId + ')>Delete</button>';
+					// cell8.innerHTML = contactId;
+					
+					
+					if( i < jsonObject.results.length - 1 )
+					{	
+						
+					}
+				}
+
+
+			}
+		};
+		xhr.send(jsonPayload);
+
+	}
+	catch(err)
+	{
+		document.getElementById("searchResults").innerHTML = err.message;
+	}
+	
+}
+
+function deleteContact( contactId )
+{
+	// document.getElementById("regResult").innerHTML = "";
+
+	//var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+	var jsonPayload = '{"userId" : "' + userId + '", "contactId" : "' + contactId + '"}';
+	var url = urlBase + '/DeleteContact.' + extension;
+	
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+	try
+	{
+		xhr.send(jsonPayload);
+		// var jsonObject = JSON.parse( xhr.responseText );
+		// userId = jsonObject.id;
+
+		// if( userId < 1  )
+		// {
+		//  	document.getElementById("regResult").innerHTML = "E-mail already exists!";
+		//  	return;
+		// }
+
+		// document.getElementById("printHere") = jsonPayload;
+		// firstName = jsonObject.firstName;
+		// lastName = jsonObject.lastName;
+
+		// 	saveCookie();
+
+
+		alert("Contact Deleted!");
+		window.location.href = "dashboard.html";
+
+		
+	}
+	catch(err)
+	{	
+		alert("Error" + err.message);
+	}
+}
+
 function saveCookie()
 {
 	    var minutes = 20;
@@ -215,138 +358,3 @@ function readCookie()
 	{
 	}
 }
-
-function searchContact()
-{
-	var srch = document.getElementById("searchText").value;
-	//document.getElementById("searchResults").innerHTML = "";
-
-	// Json DB elements
-	var table = document.getElementById("contactTable");
-	var name = "";
-	var email = "";
-	var city = "";
-	var state = "";
-	var zip = "";
-	var phoneNumber = "";
-	var infected = "";
-	var datecontactcreated = "";
-	var contactId = "";
-	// var btn = document.createElement("button");
-
-	// Variables for Table
-	
-
-	// Converts to JSON package + sends to the API
-	var jsonPayload = '{"search" : "' + srch + '","userId" : ' + userId + '}';
-	var url = urlBase + '/SearchContact.' + extension;
-	
-	// API Call
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{	
-				var jsonObject = JSON.parse( xhr.responseText );
-
-				// Text displays to inform client the search results are displayed. 
-				if (jsonObject.results.length == 0)
-					document.getElementById("searchResults").innerHTML = "No contacts found!";
-				else if (jsonObject.results.length == 1)
-					document.getElementById("searchResults").innerHTML = "1 contact found!";
-				else if (jsonObject.results.length > 1)
-					document.getElementById("searchResults").innerHTML = jsonObject.results.length + " contacts found!";
-
-
-				for( var i=0; i<jsonObject.results.length; i++ )
-				{
-					// Needs to convert to 
-				
-					// name += jsonObject.results[i].name;
-					// email += jsonObject.results[i].email;
-					// city += jsonObject.results[i].city;
-					// state += jsonObject.results[i].state;
-					// zip += jsonObject.results[i].zip;
-					// phoneNumber += jsonObject.results[i].phoneNumber;
-					// infected += jsonObject.results[i].infected;
-					// btn += btn;
-				
-					name = jsonObject.results[i].name;
-					email = jsonObject.results[i].email;
-					city = jsonObject.results[i].city;
-					state = jsonObject.results[i].state;
-					zip = jsonObject.results[i].zip;
-					phoneNumber = jsonObject.results[i].phoneNumber;
-					datecontactcreated = jsonObject.results[i].datecontactcreated;
-					infected = jsonObject.results[i].infected;
-
-					contactId = jsonObject.results[i].contactId;
-
-					var row = table.insertRow(-1);
-					
-					var cell0 = row.insertCell(0);
-					var cell1 = row.insertCell(1);
-					var cell2 = row.insertCell(2);
-					var cell3 = row.insertCell(3);
-					var cell4 = row.insertCell(4);
-					var cell5 = row.insertCell(5);
-					var cell6 = row.insertCell(6);
-					var cell7 = row.insertCell(7);
-					var cell8 = row.insertCell(8);
-					
-					
-					cell0.innerHTML = name;
-					cell1.innerHTML = email;
-					cell2.innerHTML = city;
-					cell3.innerHTML = state;
-					cell4.innerHTML = zip;
-					cell5.innerHTML = phoneNumber;
-					cell6.innerHTML = infected;
-					cell7.innerHTML = datecontactcreated;
-					cell8.innerHTML = '<button type="button" class="btn btn-primary">Edit</button><button type="button" class="btn btn-dark">Delete</button>';
-					// cell8.innerHTML = contactId;
-					
-					
-					if( i < jsonObject.results.length - 1 )
-					{	
-						// name += "<br />\r\n";
-						// email += "<br />\r\n";
-						// city += "<br />\r\n";
-						// state += "<br />\r\n";
-						// zip += "<br />\r\n";
-						// phoneNumber += "<br />\r\n";
-						// infected += "<br />\r\n";
-						// btn += "<br />\r\n";
-					
-						
-						// do the same thing with all the field
-					}
-				}
-				// document.getElementById("print_sector").innerHTML = name;
-				// document.getElementById("nameField").innerHTML = name;
-				// document.getElementById("emailField").innerHTML = email;
-				// document.getElementById("cityField").innerHTML = city;
-				// document.getElementById("stateField").innerHTML = state;
-				// document.getElementById("zipField").innerHTML = zip;
-				// document.getElementById("phoneNumberField").innerHTML = phoneNumber;
-				// document.getElementById("infectedField").innerHTML = infected;
-				// document.getElementById("btnId").innerHTML = btn.type;
-				// document.getElementsByTagName("p")[i].innerHTML = test_name;
-
-			}
-		};
-		xhr.send(jsonPayload);
-
-	}
-	catch(err)
-	{
-		document.getElementById("searchResults").innerHTML = err.message;
-	}
-	
-}
-
-
