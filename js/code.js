a86924a3c8ac6d59c9859e2f502c55640e3f838c
tugitit <tule1102@gmail.com>
2020-05-30 21:33:40 -0400

    testing for added date

--- js/code.js
+++ js/code.js
@@ -288,24 +288,25 @@ function searchContact()
 
 					var row = table.insertRow(-1);
 					
-					var cell1 = row.insertCell(0);
-					var cell2 = row.insertCell(1);
-					var cell3 = row.insertCell(2);
-					var cell4 = row.insertCell(3);
-					var cell5 = row.insertCell(4);
-					var cell6 = row.insertCell(5);
-					var cell7 = row.insertCell(6);
-					var cell8 = row.insertCell(7);
-					var cell0 = row.insertCell(9);
+					var cell0 = row.insertCell(0);
+					var cell1 = row.insertCell(1);
+					var cell2 = row.insertCell(2);
+					var cell3 = row.insertCell(3);
+					var cell4 = row.insertCell(4);
+					var cell5 = row.insertCell(5);
+					var cell6 = row.insertCell(6);
+					var cell7 = row.insertCell(7);
+					var cell8 = row.insertCell(8);
 					
-					cell0.innerHTML = datecontactcreated;
-					cell1.innerHTML = name;
-					cell2.innerHTML = email;
-					cell3.innerHTML = city;
-					cell4.innerHTML = state;
-					cell5.innerHTML = zip;
-					cell6.innerHTML = phoneNumber;
-					cell7.innerHTML = infected;
+					
+					cell0.innerHTML = name;
+					cell1.innerHTML = email;
+					cell2.innerHTML = city;
+					cell3.innerHTML = state;
+					cell4.innerHTML = zip;
+					cell5.innerHTML = phoneNumber;
+					cell6.innerHTML = infected;
+					cell7.innerHTML = datecontactcreated;
 					cell8.innerHTML = '<button type="button" class="btn btn-primary">Edit</button><button type="button" class="btn btn-dark">Delete</button>';
 					// cell8.innerHTML = contactId;
 					
