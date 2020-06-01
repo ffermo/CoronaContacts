f90260b1268cb1f107169d444bbde3a3438855a9
tugitit <tule1102@gmail.com>
2020-05-31 23:04:33 -0400

    contactid

--- js/code.js
+++ js/code.js
@@ -261,7 +261,8 @@ function searchContact()
 					cell5.innerHTML = phoneNumber;
 					cell6.innerHTML = infected;
 					cell7.innerHTML = datecontactcreated;
-					cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact('+ name + ',' + email + ',' + city + ',' + state + ',' + zip + ',' + phoneNumber + ',' + infected + ',' + contactId +')">Edit</button>';
+					// cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact('+ name + ',' + email + ',' + city + ',' + state + ',' + zip + ',' + phoneNumber + ',' + infected + ',' + contactId +')">Edit</button>';
+					cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact('+ contactId +')">Edit</button>';
 					cell9.innerHTML = '<button type="button" class="btn btn-dark" onclick="deleteContact(' + contactId + ')">Delete</button>';
 					// cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact( name )">Edit</button><button type="button" class="btn btn-dark" onclick="deleteContact(' + contactId + ')">Delete</button>';
 					// cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact( name , email , city , state, zip , phoneNumber , infected , contactId )">Edit</button> <button type="button" class="btn btn-dark" onclick="deleteContact(' + contactId + ')">Delete</button>';
@@ -305,11 +306,12 @@ function deleteContact( contactId )
 	}
 }
 
-function editContact(name, email, city, state, zip, phoneNumber, infected, contactId )
-// function editContact( name )
+// function editContact(name, email, city, state, zip, phoneNumber, infected, contactId )
+function editContact( contactId )
 {
 	// console.log(name + '\n' + email + '\n' + city + '\n' + state + '\n' + zip + '\n' + phoneNumber + '\n' + infected + '\n' + contactId);
-	alert("Edit function Entered");
+	alert("Edit function Entered: " + contactId );
+
 	// if(name == null)
 	// 	name = '';
 	// else if(email == null)
