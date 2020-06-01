616dddc1f3d5c53fed4db724ecd4e7575fdfecfc
tugitit <tule1102@gmail.com>
2020-05-31 20:31:20 -0400

    testing edit button

--- js/code.js
+++ js/code.js
@@ -252,8 +252,8 @@ function searchContact()
 					cell5.innerHTML = phoneNumber;
 					cell6.innerHTML = infected;
 					cell7.innerHTML = datecontactcreated;
-					cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact('+ name + ',' + email + ',' + city + ',' + state + ',' + zip + ',' + phoneNumber + ',' + infected + ',' + contactId +')">Edit</button><button type="button" class="btn btn-dark" onclick="deleteContact(' + contactId + ')">Delete</button>';
-					// cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact()">Edit</button><button type="button" class="btn btn-dark" onclick="deleteContact(' + contactId + ')">Delete</button>';
+					// cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact('+ name + ',' + email + ',' + city + ',' + state + ',' + zip + ',' + phoneNumber + ',' + infected + ',' + contactId +')">Edit</button><button type="button" class="btn btn-dark" onclick="deleteContact(' + contactId + ')">Delete</button>';
+					 cell8.innerHTML = '<button type="button" class="btn btn-primary" onclick="editContact()">Edit</button><button type="button" class="btn btn-dark" onclick="deleteContact(' + contactId + ')">Delete</button>';
 
 					// cell8.innerHTML = contactId;
 					
