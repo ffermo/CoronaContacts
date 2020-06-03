d5aea5ebca562e2c2364f8d83231b1cdc3744bd6
faizarali <54734200+faizarali@users.noreply.github.com>
2020-06-02 22:39:09 -0400

    Added Comments

--- APICode/DeleteContact.php
+++ APICode/DeleteContact.php
@@ -1,15 +1,14 @@
 <?php
 	$inData = getRequestInfo();
-	// $ID = getID();
 
-	// $searchResults = "";
-	// NOTE: Variables (might change later)
 	// Parse and store individuals fields from JSON field into variables.
 	$ID = $inData["ID"];
 	$userId = $inData["userId"];
-	// establishing connection to localhost
+
+	// Establishing connection to localhost.
 	$conn = new mysqli("localhost", "faizar", "", "coronacontacts");
-	// returning an error if the connection was not successfull
+
+	// Returning an error if the connection was not successful.
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
@@ -17,7 +16,7 @@
 
 	else
 	{
-		// UserID included just to make sure you can't delete someone else's contacts
+		// UserID included just to make sure you can't delete someone else's contacts.
 		$sql = "DELETE FROM Contacts WHERE ID = '" . $ID . "'" . "AND UserID ='" . $userId . "'";
 		
 		if( $result = $conn->query($sql) != TRUE )
@@ -27,7 +26,7 @@
 		
 		$conn->close();
 	}
-	// Confirms that the API works
+	// Confirms that the API works.
 	returnWithError("CONTACT DELETED!");
 	
 	function getRequestInfo()
